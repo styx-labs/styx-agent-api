e61b5ac4bafcaec6c3ba3f224a116ca411cae91c
Jason He <jasonhe.md@gmail.com>
2024-11-19 13:26:06 -0500

    Whitelist agent ui

--- main.py
+++ main.py
@@ -78,7 +78,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://unilink-agent-ui-16250094868.us-central1.run.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
